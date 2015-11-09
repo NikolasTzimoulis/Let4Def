3ff84314a63346dd8e499a13172949b547f4eb6b
Nikolas <naurgul@gmail.com>
2015-11-09 23:47:06 +0100

    Included content folder

---
+++ content/panorama/scripts/custom_game/custom_round_timer.js
@@ -0,0 +1,37 @@
+"use strict";
+var TimeLimit     = 20*60;
+
+var humanTime = function(time) {
+    time = Math.floor(time);
+    var minutes = Math.floor(time / 60);
+    var seconds = time - (minutes * 60);
+
+    if (seconds < 10)
+        seconds = '0' + seconds;
+
+    return minutes + ':' + seconds;
+};
+
+
+function UpdateRoundTimer() {
+    var secondsPassed = Game.GetDOTATime(false,false);
+    var TimeLeft = TimeLimit - secondsPassed;
+    var NextRoundPercent = TimeLeft > 0 ? Math.floor((TimeLeft / TimeLimit) * 10000) / 100 : 0;
+
+    $('#RoundTimerCountDownText').text = humanTime(TimeLeft);
+    $('#RoundTimerBarPercentage').style.width = NextRoundPercent + '%';
+
+    $('#RoundTimerRoundText').text = $.Localize('timer');
+
+    $.Schedule(0.5, UpdateRoundTimer);
+}
+
+function ChangeTimeLimit(event) {
+    TimeLimit = event.timelimit;
+}
+
+/* Initialization */
+(function() {
+    GameEvents.Subscribe( "time_limit_change", ChangeTimeLimit);
+    UpdateRoundTimer();
+})();
